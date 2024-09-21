0b6f8cf25187467173ccb5cfd98bdbef53eba82b
andrii.yazvinskyi <andrii.yazvinskyi@lexima.tech>
2024-09-22 00:27:13 +0300

    set proxy true

--- data/config.py
+++ data/config.py
@@ -11,7 +11,7 @@ DELAYS = {
 }
 
 PROXY = {
-    "USE_PROXY_FROM_FILE": False,  # True - if use proxy from file, False - if use proxy from accounts.json
+    "USE_PROXY_FROM_FILE": True,  # True - if use proxy from file, False - if use proxy from accounts.json
     "PROXY_PATH": "data/proxy.txt",  # path to file proxy
     "TYPE": {
         "TG": "http",  # proxy type for tg client. "socks4", "socks5" and "http" are supported
@@ -28,7 +28,7 @@ TIMEOUT = 30
 SOFT_INFO = f"""{"TON Station".center(40)}
 Soft for https://t.me/tonstationgames_bot
 {"Functional:".center(40)}
-Register accounts in web app; start and claim farming; 
+Register accounts in web app; start and claim farming;
 
 The soft also collects statistics on accounts and uses proxies from {f"the {PROXY['PROXY_PATH']} file" if PROXY['USE_PROXY_FROM_FILE'] else "the accounts.json file"}
 To buy this soft with the option to set your referral link write me: https://t.me/Axcent_ape
